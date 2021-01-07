6400947ae9ac643cfdb1bff2e4430f66622eab55
Gosha <gosha-kyznecov@mail.ru>
2021-01-08 02:27:05 +0300

    Добавил примеры использования MySql

--- dotnet/resources/Server/MySql.cs
+++ dotnet/resources/Server/MySql.cs
@@ -10,30 +10,43 @@ namespace Server
 {
     class MySql : Script
     {
+        const string connStr = "server=localhost;user=root;database=drift;port=3306;password=";
+        MySqlConnection conn = new MySqlConnection(connStr);
         [ServerEvent(Event.ResourceStart)]
         public void MySqlConnect()
         {
-            const string connStr = "server=localhost;user=root;database=drift;port=3306;password=";
-            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
             try
             {
-                NAPI.Util.ConsoleOutput("Connecting to MySQL...");
-                conn.Open();
-
-                string sql = "SELECT Username, Password FROM accounts";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    NAPI.Util.ConsoleOutput(rdr[0] + " -- " + rdr[1]);
-                }
-                rdr.Close();
+                NAPI.Util.ConsoleOutput("[Mysql]Подключено успешно");
             }
             catch (Exception ex)
             {
-                NAPI.Util.ConsoleOutput(ex.ToString());
+                NAPI.Util.ConsoleOutput("[Mysql]Не удалось подключиться к бд. Код ошибки: " + ex.ToString());
             }
-        }  
+            //TestQuery();
+            TestExecute();
+        }
+        
+        public void TestQuery() // Пример создания нового поля в таблице. Аналогично обновление в полях.
+        {
+            string sql = "INSERT INTO `accounts`(`Username`, `Password`) VALUES ('Nico', '123')";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void TestExecute() // Пример получение данных с таблицы 
+        {
+            string sql = "SELECT Username, Password FROM accounts";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                NAPI.Util.ConsoleOutput(rdr[0] + " -- " + rdr[1]);
+            }
+            rdr.Close(); 
+        }
     }
 }
+            
