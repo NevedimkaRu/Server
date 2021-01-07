d505faf021d82990451b7acb841ea36659e9199d
Gosha <gosha-kyznecov@mail.ru>
2021-01-08 02:08:43 +0300

    Проверил подключение к MySql с помощью MySqlConnector. Всё работает, можно использовать

---
+++ dotnet/resources/Server/MySql.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+using GTANetworkAPI;
+
+
+
+namespace Server
+{
+    class MySql : Script
+    {
+        [ServerEvent(Event.ResourceStart)]
+        public void MySqlConnect()
+        {
+            const string connStr = "server=localhost;user=root;database=drift;port=3306;password=";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                NAPI.Util.ConsoleOutput("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "SELECT Username, Password FROM accounts";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    NAPI.Util.ConsoleOutput(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                NAPI.Util.ConsoleOutput(ex.ToString());
+            }
+        }  
+    }
+}
